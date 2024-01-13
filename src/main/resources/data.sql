1c0daff6517800f2b0deff8ba586cd24732f6ed2
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-01-13 11:15:40 +0100

    Create data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (id, email, first_name, last_name, password) VALUES (1, 'johnDoe@gmail.com', 'John', 'Doe', '$2a$)');
+INSERT INTO users (id, email, first_name, last_name, password) VALUES (2, 'jacquesMichel@gmail.com', 'Jacques', 'Michel', 'jnhudzb');
+INSERT INTO users (id, email, first_name, last_name, password) VALUES (3, 'moragneMouche@gmail.com', 'Moragne', 'Mouche', 'mdez');
+INSERT INTO users (id, email, first_name, last_name, password) VALUES (4, 'georgesPlo@gmail.com', 'Georges', 'Plo', 'azertyu');
